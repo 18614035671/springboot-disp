6217a94c034868e08069078eb3bf42a15ca97067
徐子泷 <18614035671@163.com>
2019-06-05 17:25:59 +0800

    Swagger2整合

--- src/main/java/com/disp/common/config/Swagger2Config.java
+++ src/main/java/com/disp/common/config/Swagger2Config.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,17 +20,19 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.disp.*.controller"))
+                //不能com.disp.*
+                .apis(RequestHandlerSelectors.basePackage("com.disp.sys.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("springboot利用swagger构建api文档")
-                .description("简单优雅的restfun风格，http://blog.csdn.net/saytime")
-                .termsOfServiceUrl("http://blog.csdn.net/saytime")
+                .title("API文档")
+                //.description("简单优雅的restfun风格，https://www.disputess.com")
+                //.termsOfServiceUrl("https://www.disputess.com")
                 .version("1.0")
+                //.contact(new Contact("X.Z.L", "https://www.disputess.com", "18614035671@163.com"))
                 .build();
     }
 }
